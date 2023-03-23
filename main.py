73141a77f93a78667d42f61bfe95fd0c7236a13e
“Vitali <abdullajondev@gmail.com>
2023-03-23 08:54:46 +0500

    added sum price and company logo and name

--- main.py
+++ main.py
@@ -23,24 +23,46 @@ for filepath in filepaths:
 
     df = pandas.read_excel(filepath, sheet_name="Sheet 1")
 
+    # add columns
     columns = df.columns
     columns = [item.replace("-", " ").title() for item in columns]
     pdf.set_font(family="Times", size=10, style="B")
     pdf.set_text_color(80, 80, 80)
     pdf.cell(w=30, h=8, txt=columns[0], border=1)
-    pdf.cell(w=70, h=8, txt=columns[1], border=1)
-    pdf.cell(w=30, h=8, txt=columns[2], border=1)
+    pdf.cell(w=60, h=8, txt=columns[1], border=1)
+    pdf.cell(w=40, h=8, txt=columns[2], border=1)
     pdf.cell(w=30, h=8, txt=columns[3], border=1)
     pdf.cell(w=30, h=8, txt=columns[4], border=1,ln=1)
 
+   # add rows
     for index, row in df.iterrows():
         pdf.set_font(family="Times", size=10)
         pdf.set_text_color(80, 80, 80)
         pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
-        pdf.cell(w=70, h=8, txt=str(row["product_name"]), border=1)
-        pdf.cell(w=30, h=8, txt=str(row["amount_purchased"]), border=1)
+        pdf.cell(w=60, h=8, txt=str(row["product_name"]), border=1)
+        pdf.cell(w=40, h=8, txt=str(row["amount_purchased"]), border=1)
         pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
         pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1,ln=1)
+
+    total_sum = df["total_price"].sum()
+    pdf.set_font(family="Times", size=10)
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=60, h=8, txt="", border=1)
+    pdf.cell(w=40, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt=str(total_sum), border=1, ln=1)
+
+    # added sum price
+    pdf.set_font(family="Times", size=10)
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt=f"The total price {total_sum}", ln=1)
+
+    # added company logo and name
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt=f"PythonHow")
+    pdf.image("pythonhow.png", w=10)
 
 
 
